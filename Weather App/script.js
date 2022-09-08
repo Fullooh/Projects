b88ba096be35f9553d22b4c16fb5e20341bd83f4
Fullooh <angstdemon1@aol.com>
2022-09-08 18:58:12 -0400

    Update to repo

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,9 +1,9 @@
 let weather = {
     "apiKey": "15d596f5221bbde8c76bd885700a366a",
-    fetchWeather: function() {
+    fetchWeather: function(lat , lon) {
         fetch("https://api.openweathermap.org/data/2.5/weather?lat="
-        + {lat} & "lon=" 
-        + {lon}
+        + lat + "&lon=" 
+        + lon
         + "&units=imperial&appid=" 
         + this.apiKey
         )
@@ -11,6 +11,6 @@ let weather = {
         .then((data) => console.log(data));
     },
     displayWeather: function(data){
-
+        console.log(data)
     }
 };
